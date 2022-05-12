3479f5c8c85c048c0b73bbac95684d97976e8a36
Israelbl <israelbl2016@gmail.com>
2022-05-11 22:13:05 -0300

    first commit

---
+++ parte1/index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<!-- META TAGS -->
+
+<meta charset="UTF-8">
+<meta property="og:url" content="https://www.superhomens.com.br/" />
+<meta property="og:type" content="website" />
+<meta property="og:title" content="Portal Saúde Masculina" />
+<meta property="og:description" content="Portal Saúde Masculina" />
+<meta property="og:image" content="https://www.superhomens.com.br/" />  
+
+<meta name="description" content="Portal Saúde Masculina">
+<meta name="keywords" content="HTML, CSS, JavaScript">
+<meta name="author" content="Portal Saúde Masculina">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+  
+<title>Pesquisa Rápida #1</title>
+
+<!-- LINKS -->
+
+<link rel="stylesheet" href="../scripts/styles.css">
+<link rel="shortcut icon" href="../images/fav.png">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;900&display=swap" rel="stylesheet">
+  
+<!-- SCRIPTS -->
+  
+<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js'></script>
+
+<!-- FACEBOOK PIXEL   -->
+
+<script>
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '352641595974282');
+    fbq('track', 'PageView');
+    fbq('track', 'QuizParte1');
+</script>
+<noscript><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=352641595974282&ev=PageView&noscript=1"/></noscript>
+
+</head> 
+<body>
+    
+<section id="notice">
+  <div class="content center">
+
+	<span>Pesquisa rápida antes de continuar...</span>
+
+  </div>
+</section>
+    
+<header>
+  <div class="content center">
+
+	<h1>Você é Maior de Idade?</h1>
+	<span class="small">Toque em uma das opções abaixo.</span>
+	
+    <a style="margin-top: 40px" class="yes" href="../parte2/"><img class="check" src="../images/check.png">Sim</a>
+    <a class="no" href="../parte2/"><img class="check" src="../images/check.png">Não</a>
+    
+    <p style="margin-top: 30px; color: #FFF;" class="center small"><a style="color: #CCC" href="../parte2/">Prefiro não responder.</a></p>
+
+  </div>
+</header>
+
+<footer>
+  <div class="content center">
+
+	<p><strong>&copy; 2022 Portal Saúde Masculina</strong><br />Todos os direitos reservados.</p>
+	<p>Este site não possui nenhuma relação com o Facebook ou com qualquer outro parceiro do Facebook. Depois que você sai do Facebook, a responsabilidade não é mais deles, e sim da empresa responsável pelo site de destino.</p>
+
+  </div>
+</footer>
+
+</body>
+</html>
